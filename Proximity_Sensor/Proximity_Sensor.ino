b2ecf001fbe408c40c587ef9f9411e76dc872218
Melanie <melanie@roberts-mbp.attlocal.net>
2018-01-19 14:58:18 -0800

    Added GPL header

--- Proximity_Sensor/Proximity_Sensor.ino
+++ Proximity_Sensor/Proximity_Sensor.ino
@@ -1,3 +1,24 @@
+/* 
+ * Proximity_Sensor reads the analog values from the Sharp IR Proximity 
+ * Sensor and uses an exponential smoothing filter to remove noise from 
+ * the sensor values.
+ * 
+ * This file is part of IR Proximity Sensor (https://github.com/melaniecabalu/IR_Proximity_Sensor).
+ * Copyright (c) 2018 Melanie Cabalu
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 int sensorPin = 0;
 float currentValue;
 float weight = 0.2;
